a387faf2019e4dafcc97e4a8b35767a5716497e1
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-07-18 09:35:48 +0530

    full name

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
   const lastname = "Smith";
   const age = "30";
   const country = "Sri Lanka";
-  
+
   return (
    <>
    <div className="cont">
    
     <h1>Employee Details</h1>
     <p> First name : John Alan Smith </p>
-    <p>Age:30</p>
+    <p>Age:{age}</p>
     <p>Country : Sri Lanka</p>
   
    </div>
