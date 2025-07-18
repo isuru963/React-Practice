0d319a6eae539e2cf9e82cd870de57f76301fb5d
Isuru S. Aththanayake <isuruaththanayake00@gmail.com>
2025-07-18 07:13:09 +0530

    stg2.1

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,16 @@
 function App() {
 
+  const a = 50;
+
   return (
    <>
    <div className="cont">
    <h1 className="heading">Isuru React</h1>
 
    <p> {10*5}% for JSX</p>
+   <p>{a}</p>
+   <p> { a>45 ? "Pass" : "Fail"}</p>
+
 
    </div>
    </>
